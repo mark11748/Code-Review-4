58efc42773d5a4f7d44368f435834c454711b19a
Mark Woodward <markwood117@gmail.com>
2017-07-17 13:46:18 -0700

    finished 1st draft of js script

--- js/script1.js
+++ js/script1.js
@@ -2,7 +2,7 @@ function Pizza(size,meats,veggies) {
 	this.size    = size;    //int : 1 to 3
 	this.meats   = meats;   //array of bools [pepperoni,sausage,anchovies,carp,haddock,seal,orangeRuffie]
 	this.veggies = veggies; //array of bools [bell,onion,mushroom,olive,spinach,tomato,pineapple,hotPepper]
-
+  this.price   = 0;
  }
 
  Pizza.prototype.price() {
@@ -21,3 +21,13 @@ function Pizza(size,meats,veggies) {
  	}
  	this.price = ((size*6)+(vegPrice*1.5)+(meatPrice*2.5)).toFixed(2);
 }
+
+$(document).ready(function(){
+  $("button#ORDER").submit(function(event){
+    var myPizza = Pizza($("input#SIZE"),$("input#MEAT"),$("input#VEG"));
+    myPizza.price();
+    $("#RECIEPT").text("TOTAL : "+myPizza.price.toString());
+    event.preventDefault();
+  })
+
+});
