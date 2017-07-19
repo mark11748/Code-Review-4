d0c14fd2ce452a3b4c7ae15a2d5cc575a77fce3c
Mark Woodward <markwood117@gmail.com>
2017-07-19 15:18:04 -0700

    simple css, finished js, modified html tag IDs in the selection portion

--- js/script1.js
+++ js/script1.js
@@ -2,12 +2,12 @@ function Pizza(size,meats,veggies) {
 	this.size    = size;    //int : 1 to 3
 	this.meats   = meats;   //array of bools [pepperoni,sausage,anchovies,ham]
 	this.veggies = veggies; //array of bools [bell,onion,mushroom,olive]
-  this.price   = 0;
+	this.price   = 0;
  }
-
- Pizza.prototype.price() {
- 	int meatPrice = 0;
- 	int   vegPrice = 0;
+ 
+ Pizza.prototype.setPrice() {
+ 	var meatPrice = 0;
+ 	var vegPrice  = 0;
 
  	this.meats.forEach(meat) {
  		if (meat === true) {
@@ -20,13 +20,24 @@ function Pizza(size,meats,veggies) {
  		}
  	}
  	this.price = ((size*6)+(vegPrice*1.5)+(meatPrice*2.5)).toFixed(2);
+	return 0;
 }
 
 $(document).ready(function(){
+
+	$(".reciept_group").hide();
   $("button#ORDER").submit(function(event){
-    var myPizza = Pizza($("input#SIZE"),$("input#MEAT"),$("input#VEG"));
-    myPizza.price();
-    $("#RECIEPT").text("<li>TOTAL : "+myPizza.price.toString()+"</li>");
+
+		var meatsChosen   = new array [$("MEAT1").prop('checked'),$("MEAT2").prop('checked'),$("MEAT3").prop('checked'),$("MEAT4").prop('checked')];
+		var veggiesChosen = new array [$("VEG1").prop('checked'),$("VEG2").prop('checked'),$("VEG3").prop('checked'),$("VEG4").prop('checked')];
+
+		var myPizza = new Pizza(parseInt($("input#SIZE").val()),meatsChosen,veggiesChosen);
+
+    myPizza.setPrice();
+
+    $("#RECIEPT").text("<li>TOTAL : $"+myPizza.price.toString()+"</li>");
+		$(".reciept_group").show();
+
     event.preventDefault();
   })
 
